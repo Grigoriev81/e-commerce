d6dbc7b08e62d57fac00e4afb1700f6d899b6191
SERHII HRYHORIEV <77696659+Grigoriev81@users.noreply.github.com>
2024-10-03 10:13:44 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://12-220.zp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://12-220.zp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/berdzavo/12-220.zp.ua/www/catalog/');
+define('DIR_SYSTEM', '/home/berdzavo/12-220.zp.ua/www/system/');
+define('DIR_IMAGE', '/home/berdzavo/12-220.zp.ua/www/image/');
+define('DIR_LANGUAGE', '/home/berdzavo/12-220.zp.ua/www/catalog/language/');
+define('DIR_TEMPLATE', '/home/berdzavo/12-220.zp.ua/www/catalog/view/theme/');
+define('DIR_CONFIG', '/home/berdzavo/12-220.zp.ua/www/system/config/');
+define('DIR_CACHE', '/home/berdzavo/12-220.zp.ua/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/berdzavo/12-220.zp.ua/www/system/storage/download/');
+define('DIR_LOGS', '/home/berdzavo/12-220.zp.ua/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/berdzavo/12-220.zp.ua/www/system/storage/modification/');
+define('DIR_UPLOAD', '/home/berdzavo/12-220.zp.ua/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'berdzavo.mysql.tools');
+define('DB_USERNAME', 'berdzavo_db12220');
+define('DB_PASSWORD', 'c9*YB22*gy');
+define('DB_DATABASE', 'berdzavo_db12220');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// Redis
+// define('CACHE_HOSTNAME', 'localhost');
+// define('CACHE_PORT', '6379');
+// define('CACHE_PREFIX', 'oc_');
